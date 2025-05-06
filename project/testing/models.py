b1840f38d3b64c908af65cb13d83729f80199514
dhananjay <dhananjayasharma43@gmail.com>
2025-05-06 11:27:32 +0530

    added by frontend

--- project/testing/models.py
+++ project/testing/models.py
@@ -7,3 +7,9 @@ class TestModel(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Frontend(models.Model):
+    name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
